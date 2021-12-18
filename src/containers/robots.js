fede355e3fd8e16b55fcadb6613be66b89869c10
Vishal-gupta98 <vishalgupta456.vg@gmail.com>
2021-12-18 21:53:57 +0530

    robofriends App

---
+++ src/containers/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Bret',
+    email: 'dennis@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Charit',
+    username: 'Antonette',
+    email: 'charit@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Divyansh',
+    username: 'Samantha',
+    email: 'Divyansh@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Rohit',
+    username: 'Karianne',
+    email: 'rohit.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Sagarva',
+    username: 'Kamren',
+    email: 'sagarva_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Dev',
+    username: 'Leopoldo_Corkery',
+    email: 'dev_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Vishal',
+    username: 'Maxime_Nienow',
+    email: 'vishal@rosamond.org'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Mohit',
+    username: 'Delphine',
+    email: 'mohit_McDermott@dana.io'
+  },
+  {
+    id: 12,
+    name: 'Clementina',
+    username: 'Moriah.Stanton',
+    email: 'Rey.adberg@karina.biz'
+  }
+];
+export default robots;
